cf12a99264c1783e4175c7e526a103560a4947db
a1113361 <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>
2025-07-19 08:42:33 +0000

    修正錯誤嵌入的 Git 專案，並更新 LB 程式碼

--- LB/app.py
+++ LB/app.py
@@ -45,5 +45,5 @@ def handle_message(event):
     line_bot_api.reply_message(event.reply_token, reply)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8081))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
